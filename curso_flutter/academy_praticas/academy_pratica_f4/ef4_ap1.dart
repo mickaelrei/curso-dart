78b44fdd7c36329c38d7c56c52bcec2db3864196
mickaelrei <mickael.reichert@gmail.com>
2023-07-25 11:13:44 -0300

    Mudanças

--- curso_flutter/academy_praticas/academy_pratica_f4/ef4_ap1.dart
+++ curso_flutter/academy_praticas/academy_pratica_f4/ef4_ap1.dart
@@ -1,9 +1,10 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 const Color darkBlue = Color.fromARGB(255, 18, 32, 47);
 
 // Lista de cores
-const colors = <String, Color>{
+final colors = <String, Color>{
   "Blue": Colors.blue,
   "Red": Colors.red,
   "Green": Colors.green,
@@ -16,6 +17,14 @@ const colors = <String, Color>{
 };
 
 void main() {
+  final random = Random();
+
+  // Adicionando cores aleatórias (HSLColor para cores vibrantes)
+  for (int i = 0; i < 15; i++) {
+    colors['Random $i'] =
+        HSLColor.fromAHSL(1.0, random.nextDouble() * 360, 1.0, 0.5).toColor();
+  }
+
   runApp(const MyApp());
 }
 
@@ -29,18 +38,15 @@ class MyApp extends StatelessWidget {
 
     // Define rota pra cada cor
     colors.forEach((nome, Color cor) {
-      rotas['/$nome'] = (context) => MaterialApp(
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: cor),
-            home: Scaffold(
-              body: Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Quando clicado, volta para a rota anterior
-                    Navigator.pop(context);
-                  },
-                  child: const Text("Voltar"),
-                ),
+      rotas['/$nome'] = (context) => Scaffold(
+            backgroundColor: cor,
+            body: Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  // Quando clicado, volta para a rota anterior
+                  Navigator.pop(context);
+                },
+                child: const Text("Voltar"),
               ),
             ),
           );
@@ -56,34 +62,29 @@ class MyApp extends StatelessWidget {
 
     // Adiciona rota padrão
     rotas['/'] = (context) => Scaffold(
-          body: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // Cria um botão pra cada cor no map
-                  for (final entry in sortedEntries)
-                    ElevatedButton(
-                      style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStatePropertyAll(entry.value)),
-                      onPressed: () {
-                        // Quando clicado, entra na rota
-                        Navigator.pushNamed(context, "/${entry.key}");
-                      },
-                      child: Text(
-                        entry.key,
-                        // Muda a cor do texto de acordo com a luminosidade da cor
-                        style: TextStyle(
-                            color: HSVColor.fromColor(entry.value).value > .5
-                                ? Colors.black
-                                : Colors.white),
-                      ),
-                    )
-                ],
-              ),
+          body: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: ListView(
+              children: [
+                // Cria um botão pra cada cor no map
+                for (final entry in sortedEntries)
+                  ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStatePropertyAll(entry.value)),
+                    onPressed: () {
+                      // Quando clicado, entra na rota
+                      Navigator.pushNamed(context, "/${entry.key}");
+                    },
+                    child: Text(
+                      entry.key,
+                      // Muda a cor do texto de acordo com a luminosidade da cor
+                      style: TextStyle(
+                          color: HSVColor.fromColor(entry.value).value > .5
+                              ? Colors.black
+                              : Colors.white),
+                    ),
+                  )
+              ],
             ),
           ),
         );
@@ -97,14 +98,5 @@ class MyApp extends StatelessWidget {
       initialRoute: "/",
       routes: rotas,
     );
-  }
-}
-
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
